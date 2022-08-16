2aa23eeaab7881135aae9bfcbbd8faa2a2614a03
gelilaa <gelila.ali@outlook.com>
2022-08-16 18:18:17 +0200

    feature: added api and components

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
+import {Search} from"./component/Search.jsx"
+import {Footer} from"./component/Footer.jsx"
+import {useLocation,Route,Routes}from 'react-router-dom';
+import {CardDetails} from "./component/CardDetails.jsx"
 
 function App() {
+  const location = useLocation()
+  var strurl = location.pathname.substring("/".length);
+ 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Routes>
+        <Route path="/" element={<Search />} />
+        <Route path={strurl}element={<CardDetails/>} />
+      </Routes>
+      <Footer />
     </div>
   );
 }
