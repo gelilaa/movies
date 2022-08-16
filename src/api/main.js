c7cd0ea68e004386cdac23ee795fd881f9677ea3
gelilaa <gelila.ali@outlook.com>
2022-08-16 19:03:32 +0200

    fix: key access for netlify

--- src/api/main.js
+++ src/api/main.js
@@ -1,7 +1,8 @@
 
 export const fetchApi =async ()=>{
+  const apiKey = process.env.REACT_APP_API_KEY;
  const data = await fetch(
-   `https://api.themoviedb.org/3/discover/movie?api_key=${process.env.REACT_APP_API_KEY}`
+   `https://api.themoviedb.org/3/discover/movie?api_key=${apiKey}`
  );
    const dataJ = await data.json();
    return dataJ;   
