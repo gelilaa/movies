e1dbf8ea4f738c72d3e66ae3ad56097bd1f3e74e
gelilaa <gelila.ali@outlook.com>
2022-08-17 13:42:24 +0200

    change access

--- src/api/search.js
+++ src/api/search.js
@@ -1,8 +1,8 @@
 
 export const searchApi = async (name) => {
-  const apiKey = process.env.REACT_APP_API_KEY;
+  //const apiKey = process.env.REACT_APP_API_KEY;
   const data = await fetch(
-    `https://api.themoviedb.org/3/search/movie?api_key=${apiKey}&language=en-US&query=${name}&page=1&include_adult=false`
+    `https://api.themoviedb.org/3/search/movie?api_key=${process.env.REACT_APP_API_KEY}&language=en-US&query=${name}&page=1&include_adult=false`
   );
   const dataJ = await data.json();
   return dataJ;
